bed9343aa0211f96d511878bf351cba375dd68dc
Olinda <olindahernandez27@gmail.com>
2023-05-19 16:05:39 -0400

    estilos

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ productos.forEach((producto) => {
     div.innerHTML = `
                     <img src=${producto.img}>
                     <h3>${producto.nombre}</h3>
-                    <p>$${producto.precio}</p>
+                    <p>$${String(new Intl.NumberFormat('de-DE').format(producto.precio))}</p>
                     <button id="agregar${producto.id}" class="boton-agregar">Agregar al carrito <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
                     <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/></svg></button>
     `
@@ -59,7 +59,8 @@ productos.forEach((producto) => {
 
 const agregarAlCarrito = (prodId) => {
     const item = productos.find((prod) => prod.id === prodId)
-    carrito.push(item)
+    const carritoItem = carrito.find((prod) => prod.id === prodId)     
+        typeof carritoItem != "undefined" ? carritoItem.cantidad++ : carrito.push(item)
     mensaje("Se ha agregado un producto al carrito.")
     actualizarCarrito()
     console.log(carrito)
@@ -81,8 +82,8 @@ const actualizarCarrito = () => {
         div.className = ('productoEnCarrito')
         div.innerHTML = `
                         <p>Producto: ${prod.nombre}</p>
-                        <p>Precio: $${prod.precio}</p>
-                        <p>Cantidad: 1 </span></p>
+                        <p>Precio: $${String(new Intl.NumberFormat('de-DE').format(prod.precio))}</p>
+                        <p>Cantidad:${prod.cantidad}</span></p>
                         <button onClick = "eliminarDelCarrito(${prod.id})" class="boton-eliminar"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
                         <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
@@ -92,5 +93,6 @@ const actualizarCarrito = () => {
         localStorage.setItem('carrito', JSON.stringify(carrito))
     })
 
-    precioTotal.innerText = carrito.reduce((acc, prod) => acc + prod.precio, 0)
+    precioTotal.innerText = carrito.reduce((acc, prod) => acc + (prod.precio * prod.cantidad), 0)
 }
+
--- productos.js
+++ productos.js
@@ -3,97 +3,113 @@ const productos = [
         id:1,
         nombre:"Bastidor",
         precio:4990,
-        img:"./imag/accesorios/bastidor.png"
+        img:"./imag/accesorios/bastidor.png",
+        cantidad:1
     },
     {
         id:2,
         nombre:"Cinta Métrica",
         precio:4990,
-        img:"./imag/accesorios/cinta-metrica.png"
+        img:"./imag/accesorios/cinta-metrica.png",
+        cantidad:1
     },
     {
         id:3,
         nombre:"Dedal de Silicona",
         precio:2590,
-        img:"./imag/accesorios/dedal-silicona.png"
+        img:"./imag/accesorios/dedal-silicona.png",
+        cantidad:1
     },
     {
-        id:4,
+        id:4,  
         nombre:"Descosedor",
         precio:2290,
-        img:"./imag/accesorios/descosedor.png"
+        img:"./imag/accesorios/descosedor.png", 
+        cantidad:1
     },
     {
         id:5,
         nombre:"Hilo Invisible",
         precio:2990,
-        img:"./imag/hilos/hilo-invisible.png"
+        img:"./imag/hilos/hilo-invisible.png",
+        cantidad:1
     },
     {
         id:6,
         nombre:"Hilo Nokton",
         precio:4990,
-        img:"./imag/hilos/hilo-nokton.png"
+        img:"./imag/hilos/hilo-nokton.png",
+        cantidad:1
     },
     {
         id:7,
         nombre:"Hilo Talia",
         precio:1490,
-        img:"./imag/hilos/hilo-talia.png"
+        img:"./imag/hilos/hilo-talia.png",
+        cantidad:1
     },
     {
         id:8,
         nombre:"Hilo Tytan",
         precio:2290,
-        img:"./imag/hilos/hilo-tytan.png"
+        img:"./imag/hilos/hilo-tytan.png",
+        cantidad:1
     },
     {
         id:9,
         nombre:"Corta Hilachas",
         precio:3590,
-        img:"./imag/tijeras/corta-hilachas.png"
+        img:"./imag/tijeras/corta-hilachas.png",
+        cantidad:1
     },
     {
         id:10,
         nombre:"Tijera de Bordado",
         precio:9990,
-        img:"./imag/tijeras/tijera-bordado.png"
+        img:"./imag/tijeras/tijera-bordado.png",
+        cantidad:1
     },
     {
         id:11,
         nombre:"Tijera de Costura",
         precio:16990,
-        img:"./imag/tijeras/tijera-costura.png"
+        img:"./imag/tijeras/tijera-costura.png",
+        cantidad:1
     },
     {
         id:12,
         nombre:"Tijera Garza",
         precio:1190,
-        img:"./imag/tijeras/tijera-garza.png"
+        img:"./imag/tijeras/tijera-garza.png",
+            cantidad:1
     },
     {
         id:13,
         nombre:"Boom",
         precio:3560,
-        img:"./imag/lanas/boom.png"
+        img:"./imag/lanas/boom.png",
+        cantidad:1
     },
     {
         id:14,
         nombre:"Merino",
         precio:4990,
-        img:"./imag/lanas/merino.png"
+        img:"./imag/lanas/merino.png",
+        cantidad:1
     },
     {
         id:15,
         nombre:"Wool",
         precio:4990,
-        img:"./imag/lanas/wool.png"
+        img:"./imag/lanas/wool.png",
+        cantidad:1
     },
     {
         id:16,
         nombre:"Wool Maxi",
         precio:36990,
-        img:"./imag/lanas/wool-maxi.png"
+        img:"./imag/lanas/wool-maxi.png",
+        cantidad:1
     }
 ];
 
