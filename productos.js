f00a898dfa65b94ec32b933c85979d0dda44ce73
Olinda <olindahernandez27@gmail.com>
2023-05-19 13:10:11 -0400

    otra correción

--- productos.js
+++ productos.js
@@ -3,97 +3,97 @@ const productos = [
         id:1,
         nombre:"Bastidor",
         precio:4990,
-        img:"./imag/accesorios/bastidor.PNG"
+        img:"./imag/accesorios/bastidor.png"
     },
     {
         id:2,
         nombre:"Cinta Métrica",
         precio:4990,
-        img:"./imag/accesorios/cinta-metrica.PNG"
+        img:"./imag/accesorios/cinta-metrica.png"
     },
     {
         id:3,
         nombre:"Dedal de Silicona",
         precio:2590,
-        img:"./imag/accesorios/dedal-silicona.PNG"
+        img:"./imag/accesorios/dedal-silicona.png"
     },
     {
         id:4,
         nombre:"Descosedor",
         precio:2290,
-        img:"./imag/accesorios/descosedor.PNG"
+        img:"./imag/accesorios/descosedor.png"
     },
     {
         id:5,
         nombre:"Hilo Invisible",
         precio:2990,
-        img:"./imag/hilos/hilo-invisible.PNG"
+        img:"./imag/hilos/hilo-invisible.png"
     },
     {
         id:6,
         nombre:"Hilo Nokton",
         precio:4990,
-        img:"./imag/hilos/hilo-nokton.PNG"
+        img:"./imag/hilos/hilo-nokton.png"
     },
     {
         id:7,
         nombre:"Hilo Talia",
         precio:1490,
-        img:"./imag/hilos/hilo-talia.PNG"
+        img:"./imag/hilos/hilo-talia.png"
     },
     {
         id:8,
         nombre:"Hilo Tytan",
         precio:2290,
-        img:"./imag/hilos/hilo-tytan.PNG"
+        img:"./imag/hilos/hilo-tytan.png"
     },
     {
         id:9,
         nombre:"Corta Hilachas",
         precio:3590,
-        img:"./imag/tijeras/corta-hilachas.PNG"
+        img:"./imag/tijeras/corta-hilachas.png"
     },
     {
         id:10,
         nombre:"Tijera de Bordado",
         precio:9990,
-        img:"./imag/tijeras/tijera-bordado.PNG"
+        img:"./imag/tijeras/tijera-bordado.png"
     },
     {
         id:11,
         nombre:"Tijera de Costura",
         precio:16990,
-        img:"./imag/tijeras/tijera-costura.PNG"
+        img:"./imag/tijeras/tijera-costura.png"
     },
     {
         id:12,
         nombre:"Tijera Garza",
         precio:1190,
-        img:"./imag/tijeras/tijera-garza.PNG"
+        img:"./imag/tijeras/tijera-garza.png"
     },
     {
         id:13,
         nombre:"Boom",
         precio:3560,
-        img:"./imag/lanas/boom.PNG"
+        img:"./imag/lanas/boom.png"
     },
     {
         id:14,
         nombre:"Merino",
         precio:4990,
-        img:"./imag/lanas/merino.PNG"
+        img:"./imag/lanas/merino.png"
     },
     {
         id:15,
         nombre:"Wool",
         precio:4990,
-        img:"./imag/lanas/wool.PNG"
+        img:"./imag/lanas/wool.png"
     },
     {
         id:16,
         nombre:"Wool Maxi",
         precio:36990,
-        img:"./imag/lanas/wool-maxi.PNG"
+        img:"./imag/lanas/wool-maxi.png"
     }
 ];
 
